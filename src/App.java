e217205d3f9d099a1ad4d61e4007510c51fbbbbf
Hai Tran <hai@gmail.com>
2023-04-23 15:33:21 +0700

    ok

---
+++ src/App.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int a,n;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("nhap vao so can tinh");
+        a = sc.nextInt();
+        System.out.println("Nhap vao so mu: ");
+        n = sc.nextInt();
+        System.out.println(a+" mu "+ n+" = "+soMu(a,n));
+    }
+    public static int soMu(int a, int n) {
+        if(n == 0) {
+            return 1;
+        }else {
+            
+            if(n == 1) {
+                return a;
+            }else {
+                if(n % 2 == 0) {
+                    return soMu(a, n /2) * soMu(a,n/2);
+                }else {
+                    return soMu(a, n -1)*soMu(a, 1);
+                }
+            }
+        }
+    }
+}
